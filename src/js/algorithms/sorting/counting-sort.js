0bd42548017d228b149efa4ec7cd165d1a924daf
rvbonfimm <rogeriovilelabonfim@gmail.com>
2025-04-21 20:30:05 -0300

    feat: add counting sort alg

---
+++ src/js/algorithms/sorting/counting-sort.js
@@ -0,0 +1,38 @@
+const utils = require('../../../utils')
+
+const spec = {
+    name: 'Counting Sort',
+    complexity: 'O(n + k), onde k é o tamanho do array temporário de contagem',
+    description: 'é um algoritmo de ordenação de inteiros',
+    understandingStatus: 'Beginner',
+    loops: 0,
+    swaps: 0
+}
+
+function countingSort(array) {
+    if (array.length < 2) {
+        return array
+    }
+    const maxValue = utils.findMaxValue(array)
+    const counts = new Array(maxValue + 1)
+    array.forEach(element => {
+        if (!counts[element]) {
+            counts[element] = 0
+        }
+        counts[element]++
+    })
+    let sortedIndex = 0
+    counts.forEach((count, i) => {
+        while (count > 0) {
+            array[sortedIndex++] = i
+            count--
+        }
+    })
+    return array
+}
+
+let array = utils.createAnArray(7, 'random')
+console.log(`Original Array: ${array.join()}`)
+array = countingSort(array)
+console.log(`Sorted Array: ${array.join()}`)
+console.log(`Alg. Spec.: ${JSON.stringify(spec)}`)
--- src/utils/index.js
+++ src/utils/index.js
@@ -36,10 +36,20 @@ function createAnArray(size, mode = 'ordered') {
     }
     return []
 }
+function findMaxValue(array) {
+    let max = array[0]
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i]
+        }
+    }
+    return max
+}
 
 module.exports = {
     Compare,
     defaultCompare,
     swap,
-    createAnArray
+    createAnArray,
+    findMaxValue
 }
