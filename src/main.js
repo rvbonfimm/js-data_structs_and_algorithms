6a5b26f4b6497face013830cf7b656c3ba8354a4
rvbonfimm <rogeriovilelabonfim@gmail.com>
2025-04-23 17:09:15 -0300

    refactor: move testing file to sorting folder

--- src/main.js
+++
@@ -1,9 +0,0 @@
-const { radixSort } = require('./js/algorithms/sorting/radix-sort')
-const utils = require('./utils/index')
-
-let randomArray = utils.createAnArray(25, 'random')
-console.log(`Input: ${JSON.stringify(randomArray)}\n`);
-
-const [output, specs] = radixSort(randomArray)
-console.log(`Output: ${JSON.stringify(output)}\n`);
-console.log(`Algorith Spec.: ${JSON.stringify(specs)}`);
